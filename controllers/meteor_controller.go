5bba43969dc9c8c9e46519a1e40b3df99228f5dc
Tomas Coufal <tcoufal@redhat.com>
2021-08-26 20:52:38 +0200

    chore: cleanup RBAC and scheme after old reconcilers
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- controllers/meteor_controller.go
+++ controllers/meteor_controller.go
@@ -27,11 +27,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	meteorv1alpha1 "github.com/aicoe/meteor-operator/api/v1alpha1"
-	imagev1 "github.com/openshift/api/image/v1"
-	routev1 "github.com/openshift/api/route/v1"
 	pipelinev1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
-	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -50,18 +46,6 @@ type MeteorReconciler struct {
 //+kubebuilder:rbac:groups=tekton.dev,resources=pipelineruns,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=tekton.dev,resources=pipelineruns/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=tekton.dev,resources=pipelineruns/finalizers,verbs=update
-//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=apps,resources=deployments/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=services/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups="",resources=services/finalizers,verbs=update
-//+kubebuilder:rbac:groups=route.openshift.io,resources=routes,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=route.openshift.io,resources=routes/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=route.openshift.io,resources=routes/finalizers,verbs=update
-//+kubebuilder:rbac:groups=image.openshift.io,resources=imagestreams,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=image.openshift.io,resources=imagestreams/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=image.openshift.io,resources=imagestreams/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -131,9 +115,5 @@ func (r *MeteorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&meteorv1alpha1.Meteor{}).
 		Owns(&pipelinev1beta1.PipelineRun{}).
-		Owns(&appsv1.Deployment{}).
-		Owns(&v1.Service{}).
-		Owns(&routev1.Route{}).
-		Owns(&imagev1.ImageStream{}).
 		Complete(r)
 }
