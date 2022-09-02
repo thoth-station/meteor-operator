c3f30f2b75d37fce20b0d1390b91db084c7d319f
Christoph Görn <goern@redhat.com>
2022-09-02 20:47:57 +0200

    operator sdk update (#63)
    * update to operator-sdk v1.22.2, relocked dependencies
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :arrow_up: update direct dependencies, relocked dependencies
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :arrow_up: update controller-gen, re-run
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * check for the type of ArrayOrString, return if its a string
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * remove the use of the CaseSensitiveJSONIterator
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :arrow_up: update pre-commit hook versions
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :arrow_up: update the golang builder image version
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :pencil: add a short note about testing locally
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * :arrow_up: update the container image used for prow's precommit job
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- controllers/meteor/pipelinerun_reconciler.go
+++ controllers/meteor/pipelinerun_reconciler.go
@@ -2,6 +2,7 @@ package meteor
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
@@ -9,7 +10,6 @@ import (
 	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
-	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/apimachinery/pkg/types"
 	pointer "k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -180,7 +180,9 @@ func (r *MeteorReconciler) ReconcilePipelineRun(name string, ctx *context.Contex
 			r.Meteor.Status.Pipelines[statusIndex].Ready = "True"
 			r.Meteor.Status.Stage.Succeeded = appendUnique(r.Meteor.Status.Stage.Succeeded, resourceName)
 			if len(res.Status.PipelineResults) > 0 {
-				r.Meteor.Status.Pipelines[statusIndex].Url = res.Status.PipelineResults[0].Value
+				if res.Status.PipelineResults[0].Value.Type == "string" {
+					r.Meteor.Status.Pipelines[statusIndex].Url = res.Status.PipelineResults[0].Value.StringVal
+				}
 			}
 		} else {
 			r.Meteor.Status.Stage.Failed = appendUnique(r.Meteor.Status.Stage.Failed, resourceName)
@@ -196,12 +198,12 @@ func (r *MeteorReconciler) ownerReferences() (string, error) {
 		return "", errors.New("no Comas found")
 	}
 	allRefs := append(r.Meteor.Status.Comas, r.Meteor.GetReference(false))
-	ownerReferences, err := json.CaseSensitiveJSONIterator().Marshal(allRefs)
+	ownerReferences, err := json.Marshal(allRefs)
 	return string(ownerReferences), err
 }
 
 func (r *MeteorReconciler) externalServices() (string, error) {
-	ownerReferences, err := json.CaseSensitiveJSONIterator().Marshal(r.Shower.Spec.ExternalServices)
+	ownerReferences, err := json.Marshal(r.Shower.Spec.ExternalServices)
 	return string(ownerReferences), err
 }
 
